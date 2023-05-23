5c548d1f6ffffc3a7533bd38e796f5ff6fd64957
Ana-Maria Cusco <anamaria_cusco@yahoo.com>
2023-05-23 13:54:55 +0300

    Added implementation

---
+++ chat-server/src/main/java/com/shubh/kafkachat/controller/ChatController.java
@@ -0,0 +1,54 @@
+package com.shubh.kafkachat.controller;
+
+import com.shubh.kafkachat.constants.KafkaConstants;
+import com.shubh.kafkachat.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class ChatController {
+
+    @Autowired
+    private KafkaTemplate<String, Message> kafkaTemplate;
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @PostMapping(value = "/api/send", consumes = "application/json", produces = "application/json")
+    public void sendMessage(@RequestBody Message message) {
+        System.out.println("Kafka message");
+        System.out.println(message);
+        message.setDate(LocalDateTime.now().toString());
+        try {
+            //Sending the message to kafka topic queue
+            kafkaTemplate.send(KafkaConstants.KAFKA_TOPIC, message).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //    -------------- WebSocket API ----------------
+
+    // WebSocket API
+    @MessageMapping("/message")
+    @SendTo("/chatroom/public")
+    public Message receiveMessage(@Payload Message message){
+        return message;
+    }
+
+    @MessageMapping("/private-message")
+    public Message recMessage(@Payload Message message){
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message);
+        System.out.println(message.toString());
+        return message;
+    }
+
+}
